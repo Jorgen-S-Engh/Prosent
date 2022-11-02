4fbfaa6353f45f3c810983eaef6f15b9a59d0da2
Jorgen-S-Engh <55982088+Jorgen-S-Engh@users.noreply.github.com>
2022-11-02 22:01:47 +0100

    make 98 mode

--- js/script.js
+++ js/script.js
@@ -1,14 +1,15 @@
 const body = document.querySelector("body");
 const results = document.querySelector(".find-percent-results");
-const btnCalcProcent = document.querySelector(".btn-calc-prosent");
 const calcExplainer = document.querySelector(".calc-explainer");
 const alertSuccess = document.querySelector(".alert-success");
 const darkMode = document.querySelector(".btn-dark");
+// const btnCalcProcent = document.querySelector(".btn-calc-prosent");
+const btnCalcProcent = document.querySelector(".btn-calc-prosent")
 
 btnCalcProcent.addEventListener("click", calcProcent)
 
-function calcProcent () {
-
+function calcProcent (e) {
+    e.preventDefault();
     const inputPartNum = document.querySelector(".part-of-number").value;
     const inputWholeNum = document.querySelector(".whole-number").value;
     let sum = (inputPartNum / inputWholeNum *100).toFixed(2)
@@ -22,6 +23,31 @@ darkMode.addEventListener("click", doDarkMode)
 function doDarkMode () {
     body.classList.toggle("dark-mode")
 }
+
+
+// const nineeight = document.getElementById("98-stylesheet").disabled = true;
+
+// --------------------------- 98 Mode -----------------------------------------
+
+const nintyeight = document.getElementById("98style")
+const btn98Mode = document.querySelector(".btn-98-mode");
+
+btn98Mode.addEventListener("click", do98Mode);
+
+let nintyeightSum = 0;
+function do98Mode() {
+    nintyeightSum++;
+    reminder = nintyeightSum % 2;
+    if (reminder === 1){
+        nintyeight.disabled = undefined;
+    }else if(reminder === 0){
+        nintyeight.disabled = "disabled";
+    }
+    
+    // console.log(sum)
+}
+
+
 
 
 
