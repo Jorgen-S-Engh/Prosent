6043aaf7fbfdf1e580d6e5c0d55628a16b1ea90a
Jorgen-S-Engh <55982088+Jorgen-S-Engh@users.noreply.github.com>
2022-11-03 15:43:01 +0100

    work on windows 98 theme and started on compounded interest

--- js/script.js
+++ js/script.js
@@ -27,29 +27,50 @@ function doDarkMode () {
 
 // --------------------------- 98 Mode -----------------------------------------
 
-const nintyeight = document.getElementById("98style")
+const nintyeight = document.getElementById("98style");
 const btn98Mode = document.querySelector(".btn-98-mode");
-
+const footer98Mode = document.querySelector(".status-windows98-mode");
+const header98Mode = document.querySelector(".header-windows98-mode");
+const rmBorder = document.querySelector(".container-find-percentage")
 btn98Mode.addEventListener("click", do98Mode);
-
 let nintyeightSum = 0;
+
 function do98Mode() {
     nintyeightSum++;
     reminder = nintyeightSum % 2;
     if (reminder === 1){
         nintyeight.disabled = undefined;
-        btnCalcProcent.classList.remove("btn-primary", "btn")
+        body.classList.add("windows-mode");
+        btnCalcProcent.classList.remove("btn-primary", "btn");
+        rmBorder.classList.remove("rounded");
+        footer98Mode.innerHTML = 
+                                    `
+                                    <p class="status-bar-field">Press F1 for help</p>
+                                    <p class="status-bar-field">Slide 1</p>
+                                    <p class="status-bar-field">CPU Usage: 14%</p>
+                                    `
+        header98Mode.innerHTML = 
+                                    `                
+                                    <div class="title-bar-controls">
+                                        <button aria-label="Minimize"></button>
+                                        <button aria-label="Maximize"></button>
+                                        <button aria-label="Close"></button>
+                                    </div>
+                                    `
 
     }else if(reminder === 0){
         nintyeight.disabled = "disabled";
         btnCalcProcent.classList.add("btn-primary", "btn")
+        footer98Mode.innerHTML = "";
+        header98Mode.innerHTML = ""; 
+        rmBorder.classList.add("rounded");
+        body.classList.remove("windows-mode")
 
     }
     
-    // console.log(sum)
 }
 
 
-
+//--------------------------Calc compounded interest ----------------
 
 
